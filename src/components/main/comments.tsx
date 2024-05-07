7e2cce15c37a97093c8c34fb4d9075ad951b77b4
Speuce <mrk5199@gmail.com>
2024-05-07 12:20:02 -0600

    Finish comments

--- src/components/main/comments.tsx
+++ src/components/main/comments.tsx
@@ -4,35 +4,63 @@ import { Comment } from './comment';
 import { Textarea } from '../ui/textarea';
 import { Button } from '../ui/button';
 import { SendHorizonal } from 'lucide-react';
+import { useToast } from '../ui/use-toast';
+import { PromptComment } from '@prisma/client';
+import { type CommentWithUser } from '~/types/comment';
+import { Spinner } from '../ui/Spinner';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface ICommentsProps {
     promptId?: string | undefined;    
+    refreshState: number;
 }
 
 export function Comments (props: ICommentsProps) {
   const [loading, setLoading] = React.useState<boolean>(false);
-  const [comments, setComments] = React.useState<Comment[]>([]);
+  const [comments, setComments] = React.useState<CommentWithUser[]>([]);
+  const { toast } = useToast();
 
   React.useEffect(() => {
+    const fetchComments = async () => {
+      setLoading(true);
+      // fetch comments from server
+      if(!props.promptId) return;
+
+      const response = await fetch(`/api/comment/fetch?promptId=${props.promptId}`);
+      if(!response.ok){
+        toast({
+          title: 'Error',
+          description: 'Failed to get comments',
+          variant: 'destructive'
+        })
+      }
+      const comments = await response.json() as CommentWithUser[];
+      setComments(comments);
+      setLoading(false);
+    }
+
     setLoading(true);
-    // fetch comments from server
-    // setComments(response.comments)
-    setLoading(false);
-  });
+    if(props.promptId){
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
+      fetchComments();
+    }
+  }, [props.promptId, toast, props.refreshState]);
   return (
     <div>
-        <div className='text-md text-slate-900 font-bold mb-2 -mt-2'>Comments</div>
-        {/* comment submission */}
-        <div className='relative mb-3'>
-            <Textarea placeholder='Add a comment' className='w-full border border-slate-400 relative min-h-16'/>
-            <Button 
-                className='text-slate-500 rounded-md p-2 absolute bottom-0 right-0 hover:text-slate-900'
-            >
-                <SendHorizonal size={24}/>
-            </Button>
+        <div className='flex flex-col gap-2'>
+        {(loading) ? 
+          <Spinner />
+          : 
+          comments.map((comment) => (
+              <Comment 
+                key={comment.id} 
+                username={comment.author.name ?? "unknown"} 
+                comment={comment.comment} 
+                date={new Date(comment.createdAt)}
+              />
+          ))
+        }
         </div>
-        <Comment username='user1' comment='This is a comment' date={new Date()}/>
     </div>
   );
 }
--- src/components/main/prompts.tsx
+++ src/components/main/prompts.tsx
@@ -4,18 +4,55 @@ import * as React from 'react';
 import { PromptBox } from '../promptbox';
 import { Dialog, DialogContent, DialogTitle } from '../ui/dialog';
 import { Button } from '../ui/button';
-import { ChevronUp, Copy, CopyCheck } from 'lucide-react';
+import { ChevronUp, Copy, CopyCheck, SendHorizonal } from 'lucide-react';
 import { Separator } from '../ui/separator';
 import { Comments } from './comments';
+import { Textarea } from '../ui/textarea';
+import { useToast } from '../ui/use-toast';
 
 export interface IPromptsAreaProps {
     prompts: Prompt[];
+    userId?: string | undefined;
 }
 
 export function PromptsArea (props: IPromptsAreaProps) {
   const [showDetail, setShowDetail] = React.useState<boolean>(false);
   const [activePrompt, setActivePrompt] = React.useState<Prompt | null>(null);
+  const [activeCommentPrompt, setActiveCommentPrompt] = React.useState<Prompt | null>(null);
   const [copied, setCopied] = React.useState<boolean>(false);
+  const [commentText, setCommentText] = React.useState<string>('');
+  const [commentLoading, setCommentLoading] = React.useState<boolean>(false);
+  const [commentRefreshState, setCommentRefreshState] = React.useState<number>(0);
+  const { toast } = useToast();
+
+  React.useEffect(() => {
+    setActiveCommentPrompt(activePrompt);
+  }, [activePrompt]);
+
+  const submitComment = async () => {
+    if(!activePrompt || !commentText || !props.userId) return;
+    setCommentLoading(true);
+    // make request to create comment
+    const res = await fetch('/api/comment/create', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({comment: commentText, promptId: activePrompt.id})
+    });
+    if(res.ok){
+      // fetch comments again
+      setCommentRefreshState(commentRefreshState + 1);
+    }else{
+      // show error message
+      toast({
+        title: 'Error',
+        description: 'Failed to add comment',
+        variant: 'destructive'
+      })
+    }
+    setCommentLoading(false);
+  }
 
   return (
     <>
@@ -56,7 +93,25 @@ export function PromptsArea (props: IPromptsAreaProps) {
               )}
             </Button>
             <Separator className='bg-slate-400 my-4'/>
-            <Comments promptId={activePrompt?.id}/>
+            <div className='text-md text-slate-900 font-bold mb-2 -mt-2'>Comments</div>
+            {(props.userId && activePrompt) && (
+              <div className='relative mb-3'>
+                  <Textarea 
+                    placeholder='Add a comment' 
+                    className='w-full border border-slate-400 relative min-h-16' 
+                    value={commentText} 
+                    onChange={(e) => {setCommentText(e.target.value)}}
+                  />
+                  <Button 
+                      className='text-slate-500 rounded-md p-2 absolute bottom-0 right-0 hover:text-slate-900'
+                      onClick={submitComment}
+                      disabled={commentLoading}
+                  >
+                      <SendHorizonal size={24}/>
+                  </Button>
+              </div>
+            )}
+            <Comments promptId={activeCommentPrompt?.id} refreshState={commentRefreshState}/>
           </div>
         </DialogContent>
       </Dialog>
